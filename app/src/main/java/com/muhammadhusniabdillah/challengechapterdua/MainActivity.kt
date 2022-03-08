ded3a86f86b5dfc30a64c75681fc61e26b2c6e19
Taboedi <husnia322@gmail.com>
2022-03-08 19:09:02 +0700

    Initial commit

---
+++ app/src/main/java/com/muhammadhusniabdillah/challengechapterdua/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.muhammadhusniabdillah.challengechapterdua
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.viewbinding.library.activity.viewBinding
+import com.muhammadhusniabdillah.challengechapterdua.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by viewBinding()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.buttonMainGojek.setOnClickListener {
+            startActivity(Intent(this, GojekChallenge::class.java))
+        }
+//        binding.buttonMainTokped
+    }
+}
