dd54bdc9534046e03a1896a3a85d76935ab014fc
hus n i <husnia322@gmail.com>
2022-03-09 18:38:21 +0700

    sisa navbar, 10% more.

--- app/src/main/java/com/muhammadhusniabdillah/challengechapterdua/GojekChallenge.kt
+++ app/src/main/java/com/muhammadhusniabdillah/challengechapterdua/GojekChallenge.kt
@@ -2,19 +2,19 @@ package com.muhammadhusniabdillah.challengechapterdua
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.viewbinding.library.activity.viewBinding
-import androidx.constraintlayout.helper.widget.Carousel
 import com.muhammadhusniabdillah.challengechapterdua.databinding.ActivityGojekChallengeBinding
+
 
 class GojekChallenge : AppCompatActivity() {
 
     private val binding: ActivityGojekChallengeBinding by viewBinding()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gojek_challenge)
+        binding.imageIklan.clipToOutline
     }
 }
+
 
 
