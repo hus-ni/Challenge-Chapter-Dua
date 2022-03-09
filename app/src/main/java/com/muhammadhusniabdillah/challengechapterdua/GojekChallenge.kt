0319d649a15f52ae4d43d0d6961d5f82eb6a31c2
hus n i <husnia322@gmail.com>
2022-03-10 00:51:33 +0700

    gojek done 100%

--- app/src/main/java/com/muhammadhusniabdillah/challengechapterdua/GojekChallenge.kt
+++ app/src/main/java/com/muhammadhusniabdillah/challengechapterdua/GojekChallenge.kt
@@ -12,7 +12,6 @@ class GojekChallenge : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gojek_challenge)
-        binding.imageIklan.clipToOutline
     }
 }
 
